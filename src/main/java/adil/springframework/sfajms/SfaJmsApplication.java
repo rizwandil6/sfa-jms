f2476c76631481078da4c03853b3d1ef6ff3ecf7
rizwandil6 <rizwan.dil6@gmail.com>
2020-10-20 15:57:38 +0530

    Initial commit

---
+++ src/main/java/adil/springframework/sfajms/SfaJmsApplication.java
@@ -0,0 +1,13 @@
+package adil.springframework.sfajms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfaJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfaJmsApplication.class, args);
+    }
+
+}
